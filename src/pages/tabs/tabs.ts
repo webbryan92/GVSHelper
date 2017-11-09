3f717818b7a1b7304f45783e813b5b5a2e42adf1
Ryan Webb <webbryan92@gmail.com>
2017-11-09 18:05:14 -0500

    Tab Functionality, database edits

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 import { AboutPage } from '../about/about';
-import { TierPage } from '../tierDisplay/tierDisplay'
-import { NativePageTransitions, NativeTransitionOptions } from '@ionic-native/native-page-transitions';
+import { TierPage } from '../tierDisplay/tierDisplay';
 
 @Component({
+  selector: "tabs-page",
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
@@ -16,38 +16,6 @@ export class TabsPage {
   termsRoot = AboutPage;
   loaded:   boolean = false;
   tabIndex: number  = 0;
-  constructor(public navCtrl: NavController, private nativePageTransitions: NativePageTransitions) {    
-  }
-  private getAnimationDirection(index):string {
-    var currentIndex = this.tabIndex;
-
-    this.tabIndex = index;
-
-    switch (true){
-      case (currentIndex < index):
-        return('left');
-      case (currentIndex > index):
-        return ('right');
-    }
-  }
-
-  public transition(e):void {    
-    let options: NativeTransitionOptions = {
-     direction:this.getAnimationDirection(e.index),
-     duration: 1000,
-     slowdownfactor: 1,
-     slidePixels: 0,
-     iosdelay: 20,
-     androiddelay: 0,
-     fixedPixelsTop: 0,
-     fixedPixelsBottom: 48
-    };
-
-    if (!this.loaded) {
-      this.loaded = true;
-      return;
-    }
-
-    this.nativePageTransitions.slide(options);
+  constructor(public navCtrl: NavController) {    
   }
 }
