6642117520bfc637d164932a65cb919633d34837
bstrommen <bstrommen@cspring.com>
2017-11-10 19:38:29 -0500

    use snapshotChanges then unpack payload locally

--- src/services/suit.service.ts
+++ src/services/suit.service.ts
@@ -4,8 +4,8 @@ import { AngularFireDatabase } from 'angularfire2/database';
 
 @Injectable()
 export class SuitService{
-    constructor ( public afdb: AngularFireDatabase) {}    
-    getPilots(suitId): Observable<any> {               
-        return this.afdb.list('/Suits/'+ suitId + '/pilots').snapshotChanges();
+    constructor ( public afdb: AngularFireDatabase) {}
+    getPilots(suitId): Observable<any> {
+        return this.afdb.list(`/Suits/${suitId}/pilots`).valueChanges();
     }
 }
